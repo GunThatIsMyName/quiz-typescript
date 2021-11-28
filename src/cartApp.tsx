ea6c882c169050c50815bcdf7db991353d8f38cd
Junhyun <mnmthatisus@gmail.com>
2021-11-28 14:57:21 +0900

    cart

--- src/cartApp.tsx
+++ src/cartApp.tsx
@@ -1,5 +1,68 @@
-const CartApp = ()=>{
-    return <h1>hello</h1>
-}
+import { Drawer, Grid, LinearProgress } from "@material-ui/core";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import Cart from "./cartComp/cart";
+import Item from "./cartComp/item";
+import { IData } from "./cartType/cart.type";
+
+const BASE_URL = "https://fakestoreapi.com/products";
+
+const fetchData = async (): Promise<IData[]> => {
+  const response = await fetch(BASE_URL);
+  const data = response.json();
+  return data;
+};
+
+const CartApp = () => {
+  const { data, isLoading, isError } = useQuery<IData[]>("props", fetchData);
+
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as IData[]);
+
+  const getTotalItem = (item: IData[]) => null;
+  const handleAddToCart = (clickItem: IData) => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (isError) {
+    return <h1> Some thing goes wrong ... </h1>;
+  }
+  return (
+    <section>
+      <Drawer open={cartOpen} anchor="right" onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          handleRemoveFromCart={handleRemoveFromCart}
+          getTotalItem={getTotalItem}
+        />
+      </Drawer>
+      <Button onClick={() => setCartOpen(true)}>
+        item: {getTotalItem(cartItems)}
+      </Button>
+      <Grid container spacing={3}>
+        {data &&
+          data.map((item) => {
+            return (
+              <Grid item key={item.id} xs={12} sm={4}>
+                <Item item={item} handleAddToCart={handleAddToCart} />
+              </Grid>
+            );
+          })}
+      </Grid>
+    </section>
+  );
+};
+
+const Button = styled.span`
+  position: fixed;
+  top: 2rem;
+  right: 5%;
+  width: 3rem;
+  height: 3rem;
+  display: block;
+  border-radius: 50%;
+  background: teal;
+`;
 
 export default CartApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CartApp from './cartApp';
+import {QueryClient ,QueryClientProvider} from "react-query";
+
+const clietn =  new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
+    <QueryClientProvider client={clietn}>
       <CartApp />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
