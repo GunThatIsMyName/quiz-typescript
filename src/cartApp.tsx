6c023e7975a6672644180738a030dda6093f764e
Junhyun <mnmthatisus@gmail.com>
2021-11-28 12:41:23 +0900

    cart app

---
+++ src/cartApp.tsx
@@ -0,0 +1,5 @@
+const CartApp = ()=>{
+    return <h1>hello</h1>
+}
+
+export default CartApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CartApp from './cartApp';
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+      <CartApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
